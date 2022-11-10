043e7c7f5e35c8dab679646576e3c232943f6e60
AbrMain <2531533498@qq.com>
2022-11-10 18:26:53 +0800

    V1.0.0.2 优化版

--- DesktopAppDlg.cpp
+++ DesktopAppDlg.cpp
@@ -14,7 +14,6 @@
 GetDesktopMessage* GDMessage = new GetDesktopMessage;//获取桌面信息
 
 // CDesktopAppDlg 对话框
-
 CDesktopAppDlg::CDesktopAppDlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_DESKTOPAPP_DIALOG, pParent)
 {
@@ -24,6 +23,9 @@ CDesktopAppDlg::CDesktopAppDlg(CWnd* pParent /*=nullptr*/)
 void CDesktopAppDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT_TEXT, m_Edit);
+	DDX_Control(pDX, IDC_BUTTON1, m_Button1);
+	DDX_Control(pDX, IDC_BUTTON2, m_Button2);
 }
 
 BEGIN_MESSAGE_MAP(CDesktopAppDlg, CDialogEx)
@@ -36,7 +38,6 @@ END_MESSAGE_MAP()
 
 
 // CDesktopAppDlg 消息处理程序
-
 BOOL CDesktopAppDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
@@ -45,64 +46,32 @@ BOOL CDesktopAppDlg::OnInitDialog()
 	//  执行此操作
 	SetIcon(m_hIcon, TRUE);				// 设置大图标
 	SetIcon(m_hIcon, FALSE);			// 设置小图标
-	// TODO: 在此添加额外的初始化代码
 	SetWindowText("DesktopApp");		//设置窗口标题
-	//GDMessage = new GetDesktopMessage;//获取桌面信息
-
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
-// 如果向对话框添加最小化按钮，则需要下面的代码
-//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
-//  这将由框架自动完成。
-
+//绘图消息处理函数
 void CDesktopAppDlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // 用于绘制的设备上下文
-
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
-
-		// 使图标在工作区矩形中居中
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
-
-		// 绘制图标
-	    dc.DrawIcon(x, y, m_hIcon);
-	
-	}
-	else
-	{
-		CDialogEx::OnPaint();
-	}
+	CDialogEx::OnPaint();//直接调用父类的绘图消息处理函数
 }
 
-//当用户拖动最小化窗口时系统调用此函数取得光标
-//显示。
+//当用户拖动最小化窗口时系统调用此函数取得光标显示
 HCURSOR CDesktopAppDlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-
-
+//按钮1被点击处理函数(备份图标)
 void CDesktopAppDlg::OnBnClickedButton1()
 {
-	CEdit* edit_control = (CEdit*)GetDlgItem(IDC_EDIT_TEXT);	//获取文本框变量
 	CString str_set = "正在备份桌面图标...";
-	edit_control->SetWindowText(str_set);						//设置文本框显示内容
-	edit_control->UpdateData();									//刷新文本框内容
-	CButton* button = (CButton*)GetDlgItem(IDC_BUTTON1);		//获取开关变量
-	button->EnableWindow(FALSE);								//开关设为不可用
+	m_Edit.SetWindowText(str_set);								//设置文本框显示内容
+	m_Edit.UpdateData();										//刷新文本框内容
+	m_Button1.EnableWindow(FALSE);								//开关设为不可用
 	
 	//返回	0：运行正常
 	//	   -1：没有备份记录（没有还原点） 无法还原
-
 	switch (GDMessage->BackupIcon())							//备份桌面图标
 	{
 	case 0:
@@ -113,24 +82,20 @@ void CDesktopAppDlg::OnBnClickedButton1()
 		break;
 	}
 
-	edit_control->SetWindowText(str_set);						//向文本框输出内容
-	button->EnableWindow(TRUE);									//开关设为可用
+	m_Edit.SetWindowText(str_set);								//向文本框输出内容
+	m_Button1.EnableWindow(TRUE);								//开关设为可用
 }
 
-//还原桌面图标
+//按钮2被点击处理函数（还原图标）
 void CDesktopAppDlg::OnBnClickedButton2()						
 {
-	
-	CEdit* edit_control = (CEdit*)GetDlgItem(IDC_EDIT_TEXT);	//获取文本框变量
 	CString str_set = "正在还原桌面图标...";						
-	edit_control->SetWindowText(str_set);						//设置文本框显示内容
-	edit_control->UpdateData();									//刷新文本框内容
-	CButton* button = (CButton*)GetDlgItem(IDC_BUTTON2);		//获取开关变量
-	button->EnableWindow(FALSE);								//开关设为不可用
+	m_Edit.SetWindowText(str_set);								//设置文本框显示内容
+	m_Edit.UpdateData();										//刷新文本框内容
+	m_Button2.EnableWindow(FALSE);								//开关设为不可用
 
 	//返回	0：运行正常
 	//	   -1：没有备份记录（没有还原点） 无法还原
-	
 	switch (GDMessage->MoveIcon())								//还原桌面图标 并且判断返回值
 	{
 	case 0:
@@ -141,6 +106,6 @@ void CDesktopAppDlg::OnBnClickedButton2()
 		break;
 	}
 
-	edit_control->SetWindowText(str_set);						//向文本框输出内容
-	button->EnableWindow(TRUE);									//开关设为可用
+	m_Edit.SetWindowText(str_set);								//向文本框输出内容
+	m_Button2.EnableWindow(TRUE);								//开关设为可用
 }
--- DesktopAppDlg.h
+++ DesktopAppDlg.h
@@ -1,9 +1,6 @@
-﻿
-// DesktopAppDlg.h: 头文件
+﻿// DesktopAppDlg.h: 头文件
 //
-
 #pragma once
-
 
 // CDesktopAppDlg 对话框
 class CDesktopAppDlg : public CDialogEx
@@ -20,18 +17,19 @@ public:
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
 
-
 // 实现
 protected:
 	HICON m_hIcon;
-
 	// 生成的消息映射函数
 	virtual BOOL OnInitDialog();
-	//afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedButton1();
 	afx_msg void OnBnClickedButton2();
+	// 文本框控件
+	CEdit m_Edit;
+	CButton m_Button1;
+	CButton m_Button2;
 };
